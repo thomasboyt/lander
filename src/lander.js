88fe6d116cc1518d77f90c3e7afc103bcca82e7b
Thomas Boyt <me@thomasboyt.com>
2013-08-19 18:06:43 -0400

    Gravity! :D

--- src/lander.js
+++ src/lander.js
@@ -3,12 +3,18 @@ var Lander = function(_, settings) {
     this[i] = settings[i];
   }
   this.size = {x: 25, y: 25};
-  this.pos.rot = 90;
+  this.pos.rot = -90;
 
   // velocity
   this.vx = 0;
   this.vy = 0;
-  this.vel = 0;
+
+  // acceleration
+  this.ax = 0;
+  this.ay = 0;
+
+  this.thrustVelocity = 0;
+  this.thrustAccel = 0;
 };
 
 Lander.prototype.draw = function(ctx) {
@@ -21,13 +27,15 @@ Lander.prototype.draw = function(ctx) {
   ctx.restore();
 };
 
-var GRAVITY_VECTOR = calcVector(0.2, 90);
 Lander.prototype.update = function(dt) {
-  var step = dt/300;
-  if (coq.inputter.state(coq.inputter.UP_ARROW)) {
-    this.vel = this.vel - 1;
-  } 
+  var step = dt/300; // arbitrary tiny number
 
+  // Check player input
+  if (coq.inputter.state(coq.inputter.UP_ARROW)) {
+    this.thrustVelocity = 1;
+  } else {
+    this.thrustVelocity = 0;
+  }
   if (coq.inputter.state(coq.inputter.LEFT_ARROW)) {
     this.pos.rot -= 1;
   }
@@ -35,39 +43,37 @@ Lander.prototype.update = function(dt) {
     this.pos.rot += 1;
   }
 
-  // update velocity
-  
-  // step 1: compute x and y components based on current velocity and direction
-  var velXY = calcVector(this.vel, this.pos.rot);
+  // Get move vector based on thrust velocity and rotation
+  var vec = calcVector(this.thrustVelocity, this.pos.rot);
 
-  // step 2: add a gravity vector
-  velXY = addVectors(velXY, GRAVITY_VECTOR);
+  // Apply gravity
+  this.ay -= 0.05 * step;
+  vec.y -= this.ay;
 
-  // step 3: compute new velocity from gravity-changed version
-  this.vel = sgn(Math.atan2(velXY.y, velXY.x)) * Math.sqrt(Math.pow(velXY.x, 2) + Math.pow(velXY.y, 2));
+  // Apply new vector to our velocity
+  this.vx += vec.x * step;
+  this.vy += vec.y * step;
 
-  // step 4: update the x and y velocity components
-  this.vx = velXY.x;
-  this.vy = velXY.y;
+  // Apply velocity to position
+  this.pos.x += this.vx;
+  this.pos.y += this.vy;
 
-  // step 5: update position based on velocity
-  this.pos.x += this.vx * step;
-  this.pos.y += this.vy * step;
-
-  // logging
   document.getElementById("vx").innerHTML = this.vx;
   document.getElementById("vy").innerHTML = this.vy;
-  document.getElementById("vel").innerHTML = this.vel;
+  document.getElementById("vel").innerHTML = this.thrustVelocity;
   document.getElementById("x").innerHTML = this.pos.x;
   document.getElementById("y").innerHTML = this.pos.y;
   document.getElementById("rot").innerHTML = this.pos.rot;
 };
 
 Lander.prototype.collision = function(other) {
-  // do collision magic
-
+  // TODO: collision magic
+  // FOR NOW: go through things or get stuck in them
   this.pos.y = other.pos.y - this.size.y;
-  this.vel = 0;
+  this.thrustVelocity = 0;
+  this.vx = 0;
+  this.vy = 0;
+  this.ay = 0;
 };
 
 function calcVector(magnitude, angle) {
@@ -75,14 +81,6 @@ function calcVector(magnitude, angle) {
   var x = magnitude * Math.cos(rad);
   var y = magnitude * Math.sin(rad);
   return { x: x, y: y };
-}
-
-function addVectors(vec1, vec2) {
-  return { x: vec1.x + vec2.x, y: vec1.y + vec2.y };
-}
-
-function sgn(x) {
-  return (x > 0) - (x < 0);
 }
 
 export default Lander;
